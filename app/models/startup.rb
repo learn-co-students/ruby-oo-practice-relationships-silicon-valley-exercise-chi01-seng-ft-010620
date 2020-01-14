03fe4110185927a52be42fef7a474aa4ff1c1aaf
m-gizzi <matthewgizzi@verizon.net>
2020-01-13 20:56:32 -0600

    Done.

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,71 @@
 class Startup
 
+    attr_reader :founder, :domain, :name
+
+    @@all = []
+
+    def initialize(name, founder, domain)
+        @name = name
+        @founder = founder
+        @domain = domain
+        @@all << self
+    end
+
+    def pivot(name, domain)
+        @name = name
+        @domain = domain
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_founder(founder)
+        self.all.find do |startup|
+            startup.founder == founder
+        end
+    end
+
+    def self.domains
+        self.all.map do |startup|
+            startup.domain
+        end
+    end
+
+    def sign_contract(venture_capitalist, type, amount_invested)
+        FundingRound.new(self, venture_capitalist, type, amount_invested)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |funding_round|
+            funding_round.startup == self
+        end
+    end
+
+    def num_funding_rounds
+        self.funding_rounds.count
+    end
+
+    def total_funds
+        sum = 0
+        self.funding_rounds.each do |funding_round|
+            sum += funding_round.investment
+        end
+        sum
+    end
+
+    def investors
+        investors = self.funding_rounds.map do |funding_round|
+            funding_round.venture_capitalist
+        end
+        investors.uniq
+    end
+
+    def big_investors
+        self.investors.select do |investor|
+            VentureCapitalist.tres_commas_club.include?(investor)
+        end
+    end
+
+
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,55 @@
 class VentureCapitalist
 
+    attr_reader :name, :total_worth
+
+    @@all = []
+
+    def initialize(name, total_worth)
+        @name = name
+        @total_worth = total_worth
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.tres_commas_club
+        self.all.select do |vc|
+            vc.total_worth >= 1000000000
+        end
+    end
+
+    def offer_contract(startup, type, amount_invested)
+        FundingRound.new(startup, self, type, amount_invested)
+    end
+
+    def funding_rounds
+        FundingRound.all.select do |funding_round|
+            funding_round.venture_capitalist == self
+        end
+    end
+
+    def portfolio
+        startups = self.funding_rounds.map do |funding_round|
+            funding_round.startup.name
+        end
+        startups.uniq
+    end
+
+    def biggest_investment
+        sorted = self.funding_rounds.sort {|a, b| b.investment <=> a.investment}
+        sorted[0]
+    end
+
+    def investment(domain)
+        total = 0
+        self.funding_rounds.each do |funding_round|
+            if funding_round.startup.domain == domain
+                total += funding_round.investment
+            end
+        end
+        total
+    end
+
 end
